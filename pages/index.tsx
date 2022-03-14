3537e4dc8e2cb8fc4cc9a3b7f907068cf299d9d0
Kei <opusk44@gmail.com>
2022-03-14 23:05:13 +0900

    add SSR for posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,14 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from '../components/Header';
+import { sanityClient, urlFor } from '../sanity';
+import { Post } from '../typings';
 
-const Home: NextPage = () => {
+interface Props {
+  posts: [Post];
+}
+
+const Home: NextPage<Props> = ({ posts }) => {
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
@@ -36,4 +42,26 @@ const Home: NextPage = () => {
   )
 }
 
-export default Home
+export const getServerSideProps = async () => {
+  const query = `*[_type == "post"] {
+    _id,
+    title,
+    author -> {
+      name,
+      image
+    },
+    description,
+    mainImage,
+    slug
+  }`;
+
+  const posts = await sanityClient.fetch(query);
+
+  return {
+    props: {
+      posts,
+    }
+  }
+};
+
+export default Home;
