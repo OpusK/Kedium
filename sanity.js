c0ba7cc3a4469d89fe3d86b54c0cd054421b33c6
Kei <opusk44@gmail.com>
2022-03-17 00:59:21 +0900

    fixed wrong sanity client API version

--- sanity.js
+++ sanity.js
@@ -8,7 +8,7 @@ export const config = {
   NEXT_PUBLIC_SANITY_DATASET || "production",
   projectId: process.env.
   NEXT_PUBLIC_SANITY_PROJECT_ID,
-  apiVersion: "2202-03-13",
+  apiVersion: "2021-08-31",
   usdCdn: process.env.NODE_ENV === 'production',
 };
 
