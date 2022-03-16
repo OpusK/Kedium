1a6428155f93cc231be8e52ddc971a2b6f8378d4
Kei <opusk44@gmail.com>
2022-03-17 00:53:11 +0900

    add createComment API

--- pages/api/createComment.ts
+++ pages/api/createComment.ts
@@ -1,18 +1,38 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { sanityClient } from '../../sanity';
 
 const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   useCdn: process.env.NODE_ENV === "production",
   token: process.env.SANITY_API_TOKEN,
+  apiVersion: '2021-08-31',
 };
+const sanityClient = require('@sanity/client');
 const client = sanityClient(config);
 
-export default function createComment(
+export default async function createComment(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const { _id, name, email, comment } = JSON.parse(req.body);
+
+  try {
+    await client.create({
+      _type: 'comment',
+      post: {
+        _type: 'reference',
+        _ref: _id,
+      },
+      name,
+      email,
+      comment,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: `Couldn't submit comment`, err })
+  }
+
+  console.log("Comment submitted");
   res.status(200).json({ name: 'John Doe' })
 }
